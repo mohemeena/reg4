ecb011b0f4c2efb3f03d0b741a1eaa3c9e3703d4
amel-osman <132000199+amel-osman@users.noreply.github.com>
2026-03-30 12:37:17 -0400

    runserver.py

---
+++ runserver.py
@@ -0,0 +1,27 @@
+#-----------------------------------------------------------------------
+# runserver.py
+# Author: Mohemeen Ahmed and Amel Osman
+#-----------------------------------------------------------------------
+""" Server program that takes in command line arguments and begins
+our flask program. """
+
+import sys
+import argparse
+import reg3
+
+def main():
+    parser = argparse.ArgumentParser(description=
+    "The registrar application")
+    parser.add_argument("port",type=int,
+    help= "the port at which the server should listen")
+
+    args = parser.parse_args()
+
+    try:
+        reg3.app.run(host='0.0.0.0', port=args.port, debug=True)
+    except Exception as ex:
+        print(f'{sys.argv[0]}: {ex}', file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
